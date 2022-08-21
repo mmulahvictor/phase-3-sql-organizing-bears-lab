838889f7f782399a0196910c649d10bf02aa5bb1
mmulahvictor <mmulahvictor@gmail.com>
2022-08-21 17:49:20 +0300

    commit tree 51d4fe000046a6ea96211c688de43ca4ba151551

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears VALUES 
+(1,"Mr. Chocolate", 10, "M", "brown", "quiet", 1),
+(2,"Rowdy", 16, "M", "blue", "peaceful", 1),
+(3,"Tabitha", 26, "F", "red", "active", 0),
+(4,"Sargeant Brown", 9, "M", "green", "restless", 0),
+(5,"Melissa", 3, "F", "dark brown", "aggressive", 1),
+(6,"Grinch", 20, "M", "pink", "changeable", 1),
+(7,"Wendy", 22, "F", "blue", "moody", 1),
+(8,null, 15, "M", "yellow", "even-tempered", 0);
